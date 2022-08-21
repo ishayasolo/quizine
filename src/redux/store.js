981f0a32f7170cd9b0f1835d8ea10f3eabdec8d9
ishayasolo <ishaya808@gmail.com>
2022-08-21 22:31:54 +0100

    remove redux

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import quizReducer from "./features/quiz/quizSlice";
-
-export const store = configureStore({
-	reducer: {
-		quiz: quizReducer
-	}
-});
